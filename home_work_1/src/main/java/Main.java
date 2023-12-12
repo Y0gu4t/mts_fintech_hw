d40afc8af6279a72cddd89bc31962fff6b27c8a0
Yegor <y0gu4t@apx_managed.vanilla>
2023-12-12 16:56:27 +0300

    fix: edit method's name and method's local variable type

--- home_work_1/src/main/java/Main.java
+++ home_work_1/src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
         // создание объектов класса Check
@@ -5,15 +7,15 @@ public class Main {
         Check check2 = new Check(2, 55.9f, 42.575f);
         Check check3 = new Check(10, 12.2f, 59.1f);
         // вызов метода calculate для объектов check1, check2, check3
-        calculate(check1);
-        calculate(check2);
-        calculate(check3);
+        calculateAndPrintTotalCheck(check1);
+        calculateAndPrintTotalCheck(check2);
+        calculateAndPrintTotalCheck(check3);
     }
 
     // метод выводит сумму покупки без скидки и со скидкой
-    public static void calculate(Check check) {
-        float amount = check.getCount() * check.getProductSum();
-        float amountWithDiscount = amount * (1 - check.getProductDiscount()/100);
+    public static void calculateAndPrintTotalCheck(Check check) {
+        BigDecimal amount = BigDecimal.valueOf(check.getCount()).multiply(BigDecimal.valueOf(check.getProductSum()));
+        BigDecimal amountWithDiscount = amount.multiply(BigDecimal.valueOf(1 - check.getProductDiscount()/100));
         System.out.printf("\tЧек\n" +
                         "Сумма покупки без скидки: %.2f\nСумма покупки со скидкой: %.2f\n\n"
                         , amount, amountWithDiscount);
