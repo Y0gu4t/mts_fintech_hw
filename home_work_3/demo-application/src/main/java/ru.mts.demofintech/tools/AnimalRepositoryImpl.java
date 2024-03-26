29d61ade5351d6b743d32a77d3d31640958b1dc4
Yegor <y0gu4t@apx_managed.vanilla>
2024-03-26 20:59:51 +0300

    feat: upload homework 12 project

--- home_work_3/demo-application/src/main/java/ru.mts.demofintech/tools/AnimalRepositoryImpl.java
+++ home_work_3/demo-application/src/main/java/ru.mts.demofintech/tools/AnimalRepositoryImpl.java
@@ -8,7 +8,11 @@ import javax.annotation.PostConstruct;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.Period;
-import java.util.*;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.stream.Collectors;
 
 public class AnimalRepositoryImpl implements AnimalRepository {
@@ -17,13 +21,13 @@ public class AnimalRepositoryImpl implements AnimalRepository {
 
     public AnimalRepositoryImpl(CreateAnimalService createAnimalService) {
         this.createAnimalService = createAnimalService;
-        animals = new ArrayList<>();
+        animals = new CopyOnWriteArrayList<>();
     }
 
     @PostConstruct
     public void createAnimals() {
         System.out.println();
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < 40; i++) {
             animals.add(createAnimalService.createUniqueAnimal());
         }
         System.out.println();
@@ -34,7 +38,7 @@ public class AnimalRepositoryImpl implements AnimalRepository {
         return animals.stream()
                 .filter(animal -> animal.getBirthDate().getYear() % 4 == 0)
                 .filter(animal -> animal.getBirthDate().getYear() % 100 != 0 || animal.getBirthDate().getYear() % 400 == 0)
-                .collect(Collectors.toMap(
+                .collect(Collectors.toConcurrentMap(
                         animal -> String.format("%s %s", animal.getType(), animal.getName()),
                         Animal::getBirthDate,
                         (animal1, animal2) -> animal1));
@@ -47,7 +51,7 @@ public class AnimalRepositoryImpl implements AnimalRepository {
         }
         Map<Animal, Integer> olderAnimalMap = animals.stream()
                 .filter(animal -> LocalDate.now().minusYears(years).isAfter(animal.getBirthDate()))
-                .collect(Collectors.toMap(
+                .collect(Collectors.toConcurrentMap(
                         animal -> animal,
                         animal -> Period.between(animal.getBirthDate(), LocalDate.now()).getYears()));
 
@@ -57,7 +61,6 @@ public class AnimalRepositoryImpl implements AnimalRepository {
                     .map(animal -> Map.of(animal, Period.between(animal.getBirthDate(), LocalDate.now()).getYears()))
                     .orElse(Collections.emptyMap());
         }
-
         return olderAnimalMap;
     }
 
@@ -69,18 +72,19 @@ public class AnimalRepositoryImpl implements AnimalRepository {
                                 list -> list.stream()
                                         .filter(animal -> list.stream()
                                                 .filter(a -> a.equals(animal)).count() > 1)
-                                        .collect(Collectors.toList()))));
+                                        .collect(Collectors.collectingAndThen(Collectors.toList(), CopyOnWriteArrayList::new)))));
     }
 
     @Override
     public void printDuplicate() {
-        System.out.println("\nAnimal Duplicates:\n");
+        System.out.println("Animal Duplicates:");
         findDuplicate().forEach((k, v) -> System.out.println(k + " = " + v));
+        System.out.println();
     }
 
     @Override
     public void findAverageAge() {
-        System.out.printf("\nAverage animal age: %.2f\n",
+        System.out.printf("Average animal age: %.2f\n",
                 animals.stream()
                         .mapToInt(animal -> Period.between(animal.getBirthDate(), LocalDate.now()).getYears())
                         .average()
@@ -98,7 +102,7 @@ public class AnimalRepositoryImpl implements AnimalRepository {
                 .filter(animal -> LocalDate.now().minusYears(5).isAfter(animal.getBirthDate()))
                 .filter(animal -> animal.getCost().compareTo(averageAnimalCost) == 1)
                 .sorted(Comparator.comparing(Animal::getBirthDate))
-                .collect(Collectors.toList());
+                .collect(Collectors.collectingAndThen(Collectors.toList(), CopyOnWriteArrayList::new));
     }
 
     @Override
@@ -115,10 +119,10 @@ public class AnimalRepositoryImpl implements AnimalRepository {
                 .limit(minConstAnimalsListSize)
                 .map(Animal::getName)
                 .sorted(Comparator.reverseOrder())
-                .collect(Collectors.toList());
+                .collect(Collectors.collectingAndThen(Collectors.toList(), CopyOnWriteArrayList::new));
     }
 
     public List<Animal> getAnimals() {
-        return new ArrayList<>(animals);
+        return new CopyOnWriteArrayList<>(animals);
     }
 }
--- home_work_3/demo-application/src/main/java/ru.mts.demofintech/tools/AnimalScheduler.java
+++ home_work_3/demo-application/src/main/java/ru.mts.demofintech/tools/AnimalScheduler.java
@@ -6,19 +6,39 @@ import org.springframework.stereotype.Component;
 import ru.mts.demofintech.exceptions.AnimalListOutOfBoundException;
 import ru.mts.demofintech.exceptions.WrongYearException;
 
+import javax.annotation.PostConstruct;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 @Component
 public class AnimalScheduler {
     AnimalRepository animalRepository;
+    ScheduledExecutorService executorService;
 
     @Autowired
     public AnimalScheduler(AnimalRepository animalRepository) {
         this.animalRepository = animalRepository;
     }
 
-    @Scheduled(cron = "*/2 * * * * ?")
+    @PostConstruct
+    public void init() {
+        executorService = Executors.newScheduledThreadPool(2);
+        Runnable printDuplicatesRunnable = () -> {
+            System.out.println("\nThread (printDuplicates):");
+            animalRepository.printDuplicate();
+        };
+        Runnable findAverageAgeRunnable = () -> {
+            System.out.println("\nThread (findAverageAge):");
+            animalRepository.findAverageAge();
+        };
+        executorService.scheduleWithFixedDelay(printDuplicatesRunnable, 5, 5, TimeUnit.SECONDS);
+        executorService.scheduleWithFixedDelay(findAverageAgeRunnable, 10, 10, TimeUnit.SECONDS);
+    }
+
+    @Scheduled(cron = "0 * * * * ?")
     public void callAnimalRepositoryMethods() {
         System.out.println("Calling methods on a class AnimalRepository:");
-        animalRepository.printDuplicate();
         System.out.println("\nAnimals that were born on a leap year:\n");
         animalRepository.findLeapYearNames().forEach((k, v) -> System.out.println(k + " " + v));
         System.out.println("\nAnimals that are older than 5 years:\n");
@@ -27,7 +47,6 @@ public class AnimalScheduler {
         } catch (WrongYearException e) {
             System.err.println("Catch the exception: " + e + "\n");
         }
-        animalRepository.findAverageAge();
         System.out.println("\nAnimals that are older than 5 years and are more expensive than average animals cost:\n");
         animalRepository.findOldAndExpensive().forEach(System.out::println);
         System.out.println("\nThe cheapest animal names:\n");
