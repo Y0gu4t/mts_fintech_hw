dbbfab1523a4e0e91eefc7ae2a2ccb24d688e525
Yegor <y0gu4t@apx_managed.vanilla>
2024-04-16 23:14:41 +0300

    upload homework 16 (add sql scripts)

---
+++ home_work_3/demo-application/sql/create_table_animals_habitats.sql
@@ -0,0 +1,4 @@
+create table animals.animals_habitats (
+    id_animal_type int,
+    id_area int
+);
---
+++ home_work_3/demo-application/sql/create_table_provider.sql
@@ -0,0 +1,5 @@
+create table animals.provider (
+    id_provider int primary key,
+    name text,
+    phone varchar(50)
+);
